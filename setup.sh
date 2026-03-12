999502578737628095da676fd384ce9340779071
Nakken13 <185896961+Nakken13@users.noreply.github.com>
2026-03-12 16:37:48 +0100

    update v2 setup

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-chmod +x /sondes/sonde.sh
-chmod +x /sondes/sonde-cpu.py
-chmod +x /sondes/sonde-disk.py
-chmod +x /sondes/sonde-ram.sh
-chmod +x /sondes/json_builder.py
+chmod +x ./sondes/sonde.sh
+chmod +x ./sondes/sonde-cpu.py
+chmod +x ./sondes/sonde-disk.py
+chmod +x ./sondes/sonde-ram.sh
+chmod +x ./sondes/json_builder.py
