387d9c0446888b272a2e3856236e6b7ab5ad8622
Nakken13 <185896961+Nakken13@users.noreply.github.com>
2026-03-12 23:38:05 +0100

    update ram

--- sondes/json_builder.py
+++ sondes/json_builder.py
@@ -8,9 +8,9 @@ import subprocess
 
 cpu = psutil.cpu_percent(interval=1)
 disk = psutil.disk_usage('/').percent 
-ram_process = subprocess.run(["bash", "./sonde-ram.sh"], capture_output=True, text=True)
-ram = int(ram_process.stdout.strip()) 
-
+script_dir = os.path.dirname(os.path.abspath(__file__))
+ram_process = subprocess.run(["bash", os.path.join(script_dir, "sonde-ram.sh")], capture_output=True, text=True)
+ram = int(ram_process.stdout.strip())
 final_json = {
     "user": os.environ.get("USER"),
     "server": os.uname()[1],
