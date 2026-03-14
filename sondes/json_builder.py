8c4741f059e9623ff06be5982a4560bdccee74a4
Nakken13 <185896961+Nakken13@users.noreply.github.com>
2026-03-14 16:32:44 +0100

    update links

--- sondes/json_builder.py
+++ sondes/json_builder.py
@@ -5,24 +5,25 @@ import os
 import time
 import subprocess
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+DIR  = os.path.dirname(os.path.abspath(__file__))
+
 
 nbr_process = subprocess.run("who | sort | cut -d' ' -f1 | uniq | wc -l", shell=True, capture_output=True, text=True)
 nbr = int(nbr_process.stdout.strip())
 
-cpu_process = subprocess.run(["./sonde-cpu.py"], capture_output=True, text=True)
+cpu_process = subprocess.run([os.path.join(DIR, "sonde-cpu.py")], capture_output=True, text=True)
 cpu = cpu_process.stdout.strip()
 
-disk_process = subprocess.run(["./sonde-disk.py"], capture_output=True, text=True)
+disk_process = subprocess.run([os.path.join(DIR, "sonde-disk.py")], capture_output=True, text=True)
 disk = disk_process.stdout.strip()
 
-ram_process = subprocess.run(["bash", os.path.join(script_dir, "sonde-ram.sh")], capture_output=True, text=True)
+ram_process = subprocess.run(["bash", os.path.join(DIR , "sonde-ram.sh")], capture_output=True, text=True)
 ram = int(ram_process.stdout.strip())
 
 final_json = {
     "user": os.environ.get("USER"),
     "server": os.uname()[1],
-    "number of users": nbr,
+    "number_of_users": nbr,
     "data": [
         {
             "timestamp": int(time.time()),
