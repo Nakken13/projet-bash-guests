b91ac654671ca1b7f1347575e133fac7a83aa20d
Nakken13 <185896961+Nakken13@users.noreply.github.com>
2026-03-15 16:23:52 +0100

    update setup v2

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/bash
-
-for f in ./sondes/*.py; do
-    chmod +x "$f"
-done
-
-for f in ./sondes/*.sh; do
+chmod +x ./sondes/sonde.sh
+for f in ./sondes/utils/*.py; do
     chmod +x "$f"
 done
